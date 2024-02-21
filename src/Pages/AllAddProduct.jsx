605f951f41965e4cc510f3ef3690425921a91c21
unknown <freelancernayem.me@gmail.comm>
2024-02-21 11:42:55 +0600

    react-rating

--- src/Pages/AllAddProduct.jsx
+++ src/Pages/AllAddProduct.jsx
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { getAllProducts } from '../Hook/product';
+import Loader from '../Hook/Loader';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+
 
 const AllAddProduct = () => {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
 
     useEffect(() => {
         const fetchData = async () => {
-
-            const data = await getAllProducts();
-            setProducts(data);
-            console.log(products);
-
+            try {
+                const data = await getAllProducts();
+                setProducts(data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+                setLoading(false);
+            }
         };
         fetchData();
     }, []);
+    if (loading) return <Loader />;
 
     return (
         <div className='p-6 my-8'>
@@ -24,18 +34,22 @@ const AllAddProduct = () => {
                 {products.map(product => (
                     <div key={product._id} className=" max-w-xs rounded overflow-hidden shadow-lg">
                         <img className=" " src={product.productImage} alt={product.productName} />
-                        <div className="px-6 py-4">
+                        <div className="px-2 py-3">
                             <div className="font-bold text-xl mb-2">{product.productName}</div>
-                            <p className="text-gray-700 text-base">{product.productType}</p>
-                            <p className="text-gray-700 text-base">$ {product.productPrice}</p>
-                            <p className="text-gray-700 text-base">{product.productRating}</p>
+
                         </div>
-                        <div className="px-6 py-4">
+                        <div className="px-6 ">
                             <div className="font-bold text-xl mb-2">{product.productBrand}</div>
 
                         </div>
-                        <div className="px-6 py-4">
-                            <span>{product.productDetails}</span>
+                        <div className="px-6 py-3">
+                            <p className="text-gray-700 text-base font-semibold">{product.productType}</p>
+                            <p className="text-gray-700 text-base font-semibold">$ {product.productPrice}</p>
+                            <Rating
+                                style={{ maxWidth: 150 }}
+                                value={product.productRating}
+                                readOnly
+                            />
                         </div>
                     </div>
                 ))}
